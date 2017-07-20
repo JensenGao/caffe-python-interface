686f1ac65f443275afbbf7dbb033086881332c3d
Jensen Gao <921988123@qq.com>
2017-07-20 22:20:26 +0800

    Create drawNet.bat

---
+++ DrawNetStruct/drawNet.bat
@@ -0,0 +1,2 @@
+python D:\caffe-master\Build\x64\Release\pycaffe\draw_net.py D:\caffe-master\models\bvlc_reference_caffenet\train_val.prototxt C:\Users\lenovo\Desktop\CaffeNet.jpg --rankdir=LR
+pause
